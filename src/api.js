acaef9ee07c34ba2c80a846ec61ecc2f890b3b81
Khayrullo Isomiddinov <isomiddinovkhayrullo@gmail.com>
2025-01-31 12:02:52 +0100

    Fix API baseURL to use Render backend

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://moodlens-backend.onrender.com/api",
 });
+
 
 export const signup = async (userData) => {
   try {
